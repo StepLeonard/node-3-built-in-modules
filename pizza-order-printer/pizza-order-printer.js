ad526402975e8b5c74792c90f45a2f31add098a3
stepleonard <stephanie.leonard716@gmail.com>
2026-03-02 13:58:41 -0800

    pizza-order-printer revised

--- pizza-order-printer/pizza-order-printer.js
+++ pizza-order-printer/pizza-order-printer.js
@@ -11,19 +11,31 @@ import fs from "fs/promises";
 const order = process.argv[2];
 const orderNumber = process.argv[3];
 
+console.log (order)
+console.log(orderNumber);
+
 async function getPizzaOrders() {
 
   const file = await fs.readFile("data.json", "utf8");
   const pizzaOrders = JSON.parse(file);
+  
 
+  // Step 4: Check what the user wants
   if (order === "getAllOrders") {
+    console.log("Print all orders:");
 
+
+// each pizza order
     pizzaOrders.forEach(pizza => console.log(pizza));
 
   } else if (order === "getOneOrder") {
+    console.log("Print one pizza order:");
 
+    // Get one pizza from the list
     const pizza = pizzaOrders[orderNumber];
+    console.log("Selected pizza:", pizza);
 
+    // If pizza exists, print it, if not show order not found
     pizza
       ? console.log(pizza)
       : console.log("Order not found. Please enter a valid order number.");
